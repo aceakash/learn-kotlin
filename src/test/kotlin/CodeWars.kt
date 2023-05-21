494a4880ed3e755d8e40dae5efa86e3f224821c0
Akash K <akashkurdekar@gmail.com>
2023-05-21 18:43:18 +0100

    kata: hero

--- src/test/kotlin/CodeWars.kt
+++ src/test/kotlin/CodeWars.kt
@@ -8,4 +8,20 @@ internal class CodeWars {
         assertEquals("88Bifk8hB8BB8BBBB888chl8BhBfd", noSpace("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"))
         assertEquals("8aaaaaddddr", noSpace("8aaaaa dddd r     "))
     }
+
+    @Test
+    fun `hero - does the hero have enough bullets to slay all the dragons`() {
+        assertFalse { hero(0, 1) }
+        assertFalse { hero(1, 1) }
+        assertFalse { hero(2, 2) }
+        assertFalse { hero(6, 4) }
+
+
+        assertTrue { hero(0, 0) }
+        assertTrue { hero(1, 0) }
+        assertTrue { hero(2, 1) }
+        assertTrue { hero(3, 1) }
+        assertTrue { hero(6, 3) }
+        assertTrue { hero(6, 2) }
+    }
 }
